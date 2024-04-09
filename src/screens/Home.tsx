883c7c081ff3298fae7325c8de03c7de89eed18c
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-04-09 16:16:37 +0530

    Create the cards section

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import {StatusBar} from 'react-native';
 
 import {Container} from '../components/shared';
 import {colors} from '../components/colors';
-import {StatusBar} from 'react-native';
+import logo1 from '../../assets/cards/mc.png';
+import logo2 from '../../assets/cards/visa_white.png';
+import CardSection from '../components/Cards/CardSection';
 
 const HomeContainer = styled(Container)`
   background-color: ${colors.graylight};
@@ -12,9 +15,34 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: React.FC = () => {
+  const cardsData = [
+    {
+      id: 1,
+      accountNo: '7829054501',
+      balance: 30500.15,
+      alias: 'Work Debit',
+      logo: logo1,
+    },
+    {
+      id: 2,
+      accountNo: '2290501854',
+      balance: 12408.01,
+      alias: 'Personal Prepaid',
+      logo: logo2,
+    },
+    {
+      id: 3,
+      accountNo: '4857173583',
+      balance: 22006.11,
+      alias: 'School Prepaid',
+      logo: logo1,
+    },
+  ];
+
   return (
     <HomeContainer>
       <StatusBar />
+      <CardSection data={cardsData} />
     </HomeContainer>
   );
 };
