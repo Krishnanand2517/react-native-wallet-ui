10fd5ac705b4c7d84ff91efe3cb610aa6bd61723
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-04-10 02:07:18 +0530

    Add logo, splash screen, keystore

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import RootStack from './navigators/RootStack';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 700);
+  }, []);
+
   return <RootStack />;
 };
 
